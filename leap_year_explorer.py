d47dd31ccb6cde570e4f7b7e39a4e6a951044fe2
TMoiseyenko06 <140467502+TMoiseyenko06@users.noreply.github.com>
2024-08-01 16:26:02 -0400

    Update leap_year_explorer.py

--- leap_year_explorer.py
+++ leap_year_explorer.py
@@ -5,7 +5,5 @@ leap_year = False
 
 if (provided_year % 4 == 0 and not provided_year % 100 == 0) or (provided_year % 100 == 0 and provided_year % 400 == 0):
     leap_year = True
-else:
-    leap_year = False
 
 print("Leap Year: " + leap_year)
